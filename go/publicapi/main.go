80525e41af9e478f09c6ced935381c042ccc39fa
abteilung6 <moeller.tilman@gmail.com>
2025-06-01 12:59:52 +0200

    publicapi: add GET /blackberries endpoint
    Returns a static list of tasty blackberries.

--- go/publicapi/main.go
+++ go/publicapi/main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type Blackberries struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func GetBlackberries(w http.ResponseWriter, r *http.Request) {
+	blackberries := []Blackberries{
+		{ID: 1, Name: "Triple Crown"},
+		{ID: 2, Name: "Black Satin"},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(blackberries)
+}
 
 func main() {
 	r := chi.NewRouter()
@@ -14,6 +30,7 @@ func main() {
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("OK"))
 	})
+	r.Get("/blackberries", GetBlackberries)
 
 	http.ListenAndServe(":8080", r)
 }
